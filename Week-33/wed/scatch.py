fd89f26560e10cd5b64fc44dc91b291ade1ee5ab
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-01-03 20:56:18 -0500

    first commit

---
+++ Week-33/wed/scatch.py
@@ -0,0 +1,46 @@
+# Python
+
+'''
+This is a comment
+block
+in python
+
+
+Variables in python act as if we are using 'var' in javascript
+'''
+
+
+# num = 1
+
+# print(num, "<---- this num")
+
+
+# if num == 1:
+#     print('we are here')
+
+
+# # 1
+# # ""
+# # ''
+
+# arr = [1, 2, 3]
+
+# print(arr[0])
+
+
+
+# # {
+# #     "hello" : "world"
+# # }
+
+
+# if num == 1 or 0:
+
+
+# nothing = None
+
+
+
+num = 1
+
+print(f"My num is {num}")
---
+++ Week-33/wed/scratch.js
@@ -0,0 +1,38 @@
+// JAVASCRIPT
+
+
+/*
+This is a multi
+line
+comment
+
+
+*/
+
+
+// let num = 1;
+
+// console.log(num, "<---- this is num");
+
+
+// if(num === 1){
+// }
+
+// console.log('we are here')
+
+
+// if(num === 1 || 0){
+
+// }
+
+
+
+// let und = undefined
+
+// let nothing = null;
+
+
+
+let num = 1
+
+console.log(`My num is ${num}`)
