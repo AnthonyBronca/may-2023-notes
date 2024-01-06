b88042b9001c445c7f905e1366d3512aa72220b9
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-01-06 14:24:30 -0500

    added notes

--- Week-33/day-5/list-comprehensions.py
+++ Week-33/day-5/list-comprehensions.py
@@ -16,18 +16,23 @@ TIP:
 
 
 """
+my_num_list = [1, 2, 3, 4, 5]
 
 # Return a new list with all items multiplied by 2
 
-my_num_list = [1, 2, 3, 4, 5]
+multiplied_by_two = [el * 2 for el in my_num_list]
+
+# print(multiplied_by_two)
 
 # return a new list with only 2 -> [2]
 
+only2lst = [el for el in my_num_list if el == 2]
 
 # print(only2lst)
 
 # return a new list, replacing all odd numbers with 5
 
+weird_lst = [el if el % 2 == 0 else 5 for el in my_num_list]
 
 # print(weird_lst)
 
@@ -45,5 +50,7 @@ Ex: [1+4, 1+5, 1+6, 2+4, 2+5, 2+6, 3+4, 3+5, 3+6] -> [5, 6, 7, 6, 7, 8, 7, 8, 9]
 lst1 = [1,2,3]
 lst2 = [4,5,6]
 
+combined_list = [(x + n) for x in lst1 for n in lst2 ]
 
-# print(combined_list)
+
+print(combined_list)
---
+++ Week-33/day-5/tuples.py
@@ -0,0 +1,38 @@
+
+
+# restaurant -> changable: price, seating arrangement, -> should not be in a tuple
+# restaurant -> name, description, location
+
+# CAN NOT CHANGE A TUPLE
+
+# restaurant_static_info = ('Burger King', "a burger joint to get burgers")
+
+
+# restaurnt_info_lst = list(restaurant_static_info)
+# restaurnt_info_lst[1] = "A burger joint to get burgers"
+# restaurant_static_info  = tuple(restaurnt_info_lst)
+
+
+# print(restaurant_static_info)
+
+# restaurant_static_info.append()
+# print(restaurant_static_info)
+
+
+
+# tuple_test = (1,)
+
+# print(tuple_test)
+
+# lst_names = ["anthony", "edward", "jade"]
+
+# lst_tuple = tuple(lst_names)
+# print(lst_tuple)
+# tuple()
+
+
+lst = ['a', 'b', 'c']
+
+
+for idx in range(len(lst)):
+    print(idx)
