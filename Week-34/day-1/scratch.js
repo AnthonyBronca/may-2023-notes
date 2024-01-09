accb29024a8162c7067cc2222565a0f604a4c7a4
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-01-08 19:39:33 -0500

    added questions

---
+++ Week-34/day-1/scratch.js
@@ -0,0 +1,11 @@
+
+
+
+let arr = [1,2,3]
+
+// let res = arr.filter((el) => el !== 2)
+
+
+let res = arr.map((el) => el*2)
+
+console.log(res)
--- Week-34/day-1/scratch.py
+++ Week-34/day-1/scratch.py
@@ -1,13 +1,30 @@
-names = ["anthony", "andrew", "bill", "edward", "arnold", "charles"]
+# names = ["anthony", "andrew", "bill", "edward", "arnold", "charles"]
+
+
+# filtered_list = list(filter(lambda el: el == "anthony", names))
+
+# print(filtered_list)
 
 # Use filter to create a NEW list of names that start with 'a'
 
+
+# a_names = list(filter(lambda el: el[0].lower() == 'a', names))
 
 # print(a_names)
 
 # ----------------------
 # Use map to create a NEW list, where all the names are upper cased
 
+# arr = [1, 2, 3]
+
+
+# def call_back(el):
+#     return el * 2
+# names = ["anthony", "andrew", "bill", "edward", "arnold", "charles"]
+
+# upper_names = list(map(lambda el: el * 2, arr))
+
+# upper_names = list(map(lambda el: el.upper(), names))
 
 # print(upper_names)
 
@@ -17,6 +34,8 @@ names = ["anthony", "andrew", "bill", "edward", "arnold", "charles"]
 # use sorted to create a NEW list where names are in alphabetical order
 
 
+# in_order = sorted(names)
+
 # print(in_order)
 
 # ----------------------
@@ -25,6 +44,9 @@ names = ["anthony", "andrew", "bill", "edward", "arnold", "charles"]
 # in name istead of first:
 # res -> ["edward", "arnold", "bill", "charles", " andrew", "anthony"]
 # Hint: Try using a helper func
+
+
+# last_name_order = sorted(names, key=lambda name: name[-1])
 
 
 # print(last_name_order)
@@ -37,8 +59,65 @@ names = ["anthony", "andrew", "bill", "edward", "arnold", "charles"]
 # Use "all" to return true if all the names are lowercased
 # Hint: try passing in an advanced list method
 
+names = ["anthony", "andrew", "bill", "edward", "arnold", "charles"]
+
+# test = map(lambda name: name == name.lower(), names)
+# all_lowered = all(map(lambda name: name == name.lower(), names))
+# print(test)
 # print(all_lowered)
+
 
 # Use "any" to return False i none of the names start with "J"
 
-# print(any_js)
+any_js = any(map(lambda name: name[0].upper() == "B", names))
+
+
+print(any_js)
+
+
+# TRY THIS OUT BELOW
+
+"""
+Write a function that filters this list to remove all numbers that are odd.
+Use the built in Filter method. Use a Lambda function to solve this
+
+
+numbers = [1,2,3,4,5,6,7,8,9]
+Answer should look like: [2,4,6,8]
+
+
+"""
+
+
+"""
+Write a function that gets the multiple of 5 for each el in this list
+Use the built in map method. Use a Lambda function to solve this
+
+numbers2 = [1,2,3,4,5]
+
+Answer should look like: [5, 10, 15, 20, 25]
+
+
+"""
+
+"""
+Write a function that returns True if all the numbers in the numbers2 problem above are odd
+Use the built in all method.
+
+
+
+Answer should look like: False
+
+
+"""
+
+
+"""
+Write a function that returns True if any of the numbers in the numbers2 problem above are greater than 20
+Use the built in any method.
+
+
+
+Answer should look like: True
+
+"""
